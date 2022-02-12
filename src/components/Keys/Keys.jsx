52b3c9821c0b99d9f4fb5c6aed14ef71b62540a8
aviv.yaari <aviv.yaari@imperva.com>
2022-02-12 18:14:19 +0200

    Fixed Keys style import

--- src/components/Keys/Keys.jsx
+++ src/components/Keys/Keys.jsx
@@ -1,22 +1,22 @@
-import * as S from "./Keys.styled";
+import { Container, Key } from "./Keys.styled";
 
 const values = [7, 8, 9, "DEL", 4, 5, 6, "+", 1, 2, 3, "-", ".", 0, "/", "*", "RESET", "="];
 
 const Keys = ({ onKeyClick }) => {
   return (
-    <S.Container>
+    <Container>
       {values.map((value) => {
         let type;
         if (value === "DEL" || value === "RESET") type = "action";
         else if (value === "=") type = "mainAction";
         else type = "digit";
         return (
-          <S.Key key={value} value={value} type={type} onClick={() => onKeyClick(value)}>
+          <Key key={value} value={value} type={type} onClick={() => onKeyClick(value)}>
             {value}
-          </S.Key>
+          </Key>
         );
       })}
-    </S.Container>
+    </Container>
   );
 };
 
